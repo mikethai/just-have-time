98777f6edd3d9e86b5a50f865fd494bfbb3fb463
miketsai <miketsai@kkstream.com>
2022-12-30 22:03:30 +0800

    feat: change postgres driver to cloudsql postgres

--- database/connect.go
+++ database/connect.go
@@ -3,8 +3,8 @@ package database
 import (
 	"fmt"
 	"log"
-	"strconv"
 
+	_ "github.com/GoogleCloudPlatform/cloudsql-proxy/proxy/dialers/postgres"
 	"github.com/mikethai/just-have-time/config"
 	"github.com/mikethai/just-have-time/internal/model"
 	"gorm.io/driver/postgres"
@@ -17,17 +17,28 @@ var DB *gorm.DB
 // ConnectDB connect to db
 func ConnectDB() {
 	var err error
-	p := config.Config("POSTGRES_DB_PORT")
-	port, err := strconv.ParseUint(p, 10, 32)
+	// p := config.Config("POSTGRES_DB_PORT")
+	// port, err := strconv.ParseUint(p, 10, 32)
 
 	if err != nil {
 		log.Println("Port Error: " + err.Error())
 	}
 
+	var (
+		dbHost = config.Config("POSTGRES_DB_HOST")
+		dbUser = config.Config("POSTGRES_DB_USER")
+		dbPwd  = config.Config("POSTGRES_DB_PASSWORD")
+		dbName = config.Config("POSTGRES_DB_NAME")
+	)
+
 	// Connection URL to connect to Postgres Database
-	dsn := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", config.Config("POSTGRES_DB_HOST"), port, config.Config("POSTGRES_DB_USER"), config.Config("POSTGRES_DB_PASSWORD"), config.Config("POSTGRES_DB_NAME"))
+	dsn := fmt.Sprintf("host=%s user=%s dbname=%s sslmode=disable password=%s", dbHost, dbUser, dbName, dbPwd)
+
 	// Connect to the DB and initialize the DB variable
-	DB, err = gorm.Open(postgres.Open(dsn))
+	DB, err = gorm.Open(postgres.New(postgres.Config{
+		DriverName: "cloudsqlpostgres",
+		DSN:        dsn,
+	}))
 
 	if err != nil {
 		panic("failed to connect database")
